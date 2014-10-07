7fa25540e1f917db8927d505171f087dc1b90055
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 12:06:53 -0700

    Completed wordcount program

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,45 @@
+import string
 
+from sys import argv 
 
-
-import sys 
 #get our data file name from the command line (*args)
-script, file_name = sys.argv
 
-#open file
-with open(file_name) as poem_line:  
-    word_count_dict = {}
-    for line in poem_line:
-        aline = line.rstrip().split(' ')
-    print aline
-#strip nonalpha characters from item
+def main(argv): 
+    script, file_name = argv             
 
-#populate dictionary
+    with open(file_name) as poem_file: 
+    
+        word_count = {}
+    
+        nonwords = string.punctuation + string.digits
+        print 1, string.punctuation
 
-#print contents of dictionary to user
+        for line in poem_file:
+
+
+# this removes newline character and the possesive "'s"
+            aline = line.rstrip().replace("'s", "").lower()
+            
+            for ch in nonwords:
+                if ch in aline:
+                    aline = aline.replace(ch, " ")
+            
+            aline = aline.split()
+            
+            for word in aline:
+                # if word not in word_count:
+                #     word_count[word] = 1
+                # else:
+                #     word_count[word] = word_count[word] + 1
+                word_count[word] = word_count.get(word, 0 ) + 1
+
+        word_count_doc(word_count)
+
+
+def word_count_doc(word_count):
+    with open("word_count.txt", "w") as out_file:             
+        for key in sorted(word_count):
+            out_file.write("%s: %d\n" % (key, word_count[key]))
+
+if __name__ == '__main__':
+    main(argv)
